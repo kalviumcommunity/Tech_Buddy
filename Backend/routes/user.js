a7f5b333d97d55ba72505a7fdaf70ac4d481a756
guruvedhanth-s <guruvedhanths@gmail.com>
2024-03-19 15:07:08 +0530

    Authentication part-1

---
+++ Backend/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+const {signupUser} = require('../controllers/userController')
+
+
+router.post('/signup',signupUser)
+
+
+module.exports = router
